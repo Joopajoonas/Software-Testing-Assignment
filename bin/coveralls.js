18d29f843ed9e2e8b2d23e6d706fec00e9ec5eb4
Joonas Lahtinen <joonas.l.lahtinen@tuni.fi>
2022-12-14 20:03:35 +0200

    fix

---
+++ bin/coveralls.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+'use strict';
+
+import { handleInput } from '..';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+let input = '';
+
+process.stdin.on('data', chunk => {
+  input += chunk;
+});
+
+process.stdin.on('end', () => {
+  handleInput(input, err => {
+    if (err) {
+      throw err;
+    }
+  });
+});
